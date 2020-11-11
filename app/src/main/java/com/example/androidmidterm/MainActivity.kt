0084b79efc07acf9759771fa619d2317d1d0f7ad
1105788551 <47253355+1105788551@users.noreply.github.com>
2020-11-11 10:25:06 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/androidmidterm/MainActivity.kt
+++ app/src/main/java/com/example/androidmidterm/MainActivity.kt
@@ -24,11 +24,30 @@ class MainActivity : AppCompatActivity() {
         // OU Calendar url
         // TODO: change this to your url after you have endpoin
 
+         val jsonObject = JSONObject()
+        jsonObject.put("kloc", "3")
+        jsonObject.put("model", "1")
+        jsonObject.put("q1", "1")
+        jsonObject.put("q2", "1")
+        jsonObject.put("q3", "1")
+        jsonObject.put("q4", "1")
+        jsonObject.put("q5", "1")
+        jsonObject.put("q6", "1")
+        jsonObject.put("q7", "1")
+        jsonObject.put("q8", "1")
+        jsonObject.put("q9", "1")
+        jsonObject.put("q10", "1")
+        jsonObject.put("q11", "1")
+        jsonObject.put("q12", "1")
+        jsonObject.put("q13", "1")
+        jsonObject.put("q14", "1")
+        jsonObject.put("q15", "1")
+
         // OU calendar returns a Json Array, if your website returns a Json Object then use JsonObjectRequest
         val url = "https://phrasal-bond-292319.uc.r.appspot.com/"
 
         // OU calendar returns a Json Array, if your website returns a Json Object then use JsonObjectRequest
-        val stringRequest = JsonObjectRequest(Request.Method.POST, url, null,
+        val stringRequest = JsonObjectRequest(Request.Method.POST, url, jsonObject,
                 Response.Listener<JSONObject> { response ->
                     val event = response
                     // eventTitle is the id for the textbox in activity_main.xml
